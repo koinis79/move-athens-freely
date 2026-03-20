0bdfd75d3ac39dd9bb3ee636800cd649c5d93575
koinis79 <vasileios@koinis.gr>
2026-03-20 18:47:27 +0200

    feat: add SEO meta tags to equipment listing page

--- src/pages/Equipment.tsx
+++ src/pages/Equipment.tsx
@@ -1,5 +1,14 @@
+import SEOHead from "@/components/SEOHead";
 import EquipmentListing from "@/components/equipment/EquipmentListing";
 
-const Equipment = () => <EquipmentListing />;
+const Equipment = () => (
+  <>
+    <SEOHead
+      title="Mobility Equipment for Rent in Athens | Moveability"
+      description="Browse our range of wheelchairs, power wheelchairs, scooters &amp; rollators. Daily &amp; weekly rates. Delivered to your accommodation."
+    />
+    <EquipmentListing />
+  </>
+);
 
 export default Equipment;
